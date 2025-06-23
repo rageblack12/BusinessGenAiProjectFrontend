5305995a69c3e49f64b47de9f942a3b5199e01d9
Suraj Patel <suraj-patel@hcltech.com>
2025-06-23 15:09:51 +0530

    basic application setup done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h3>Home page</h3>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
