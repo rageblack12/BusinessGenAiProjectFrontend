7868931e3385aae1c3dc7206bf94baf3659e1cd5
rahulsoni10 <rahulrudra12345@gmail.com>
2025-07-09 10:54:49 +0530

    Added Backend URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 
 import dotenv from 'dotenv';
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3000/api'; 
+const API_BASE_URL = 'https://businessgenaibackend.onrender.com/api'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
