53eae3d8fcf591047a29183764dfb4e46d569f06
rahulsoni10 <rahulrudra12345@gmail.com>
2025-07-09 21:19:26 +0530

    Updated LoadingSpinner.test.jsx

--- src/__tests__/LoadingSpinner.test.jsx
+++ src/__tests__/LoadingSpinner.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 import LoadingSpinner from '../components/ui/LoadingSpinner';
 
 describe('LoadingSpinner Component', () => {
